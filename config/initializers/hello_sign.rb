a66313737c5d14fd86fdf4d04d51efd4e2b052ce
Võ Anh Duy <voanhduy1512@live.com>
2014-03-17 06:31:58 +0700

    fix bugs

--- config/initializers/hello_sign.rb
+++ config/initializers/hello_sign.rb
@@ -1,3 +1,4 @@
+require 'hello_sign'
 HelloSign.configure do |config|
   config.api_key = '6ff919afef247da1ea87c4e207bdf9ee4ecfd3e283c1aa90cf623cf1732ea303'
   config.client_id = '7f91612ee160dd56bd899cd1688bbefc'
