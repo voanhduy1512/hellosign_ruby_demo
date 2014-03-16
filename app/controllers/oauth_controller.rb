a2d57a11cb5281953325a2b451c6446ca6cb8406
Võ Anh Duy <voanhduy1512@live.com>
2014-03-16 10:14:23 +0700

    hehe

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,7 +1,6 @@
 class OauthController < ApplicationController
   def index
     @auth = HelloSign.get_oauth_token(:state => params[:state], :code => params[:code])
-    binding.pry
     render :text => @auth
   end
 end
