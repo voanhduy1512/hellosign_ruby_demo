f1ef1ff841577391b028fa20e72e8744f2ce77e3
Võ Anh Duy <voanhduy1512@live.com>
2014-03-16 09:59:50 +0700

    hehe

--- config/initializers/hello_sign.rb
+++ config/initializers/hello_sign.rb
@@ -2,4 +2,5 @@ HelloSign.configure do |config|
   config.email_address = 'anhduy@siliconstraits.vn'
   config.password = '12345678'
   config.client_id = '7f91612ee160dd56bd899cd1688bbefc'
+  config.client_secret = 'a403773af1e1aee206976257a47249cf'
 end
